30ecbb03de046010dcfd3c03e332304b921b73e0
hindian <133589737+hindian-dev@users.noreply.github.com>
2023-10-18 00:33:58 +0600

    zad 2

--- src/App.vue
+++ src/App.vue
@@ -2,44 +2,35 @@
 export default {
   data: () => {
     return {
-      product1: {
-        src: "/src/assets/img1.jpg",
-        alt: "Изображение товара 1",
-        title: "Товар 1",
-        description: "Описание товара 1",
-        price: "100 тенге",
-      },
-      product2: {
-        src: "/src/assets/img2.jpg",
-        alt: "Изображение товара 2",
-        title: "Товар 2",
-        description: "Описание товара 2",
-        price: "150 тенге",
-      },
+      name: '',
+      age: []
     };
   },
 };
 </script>
 
 <template>
-  <div class="product-card">
-    <img class="product-img" :src="product1.src" :alt="product1.alt" />
-    <h2>{{ product1.title }}</h2>
-    <p>{{ product1.description }}</p>
-    <p>Цена: {{ product1.price }}</p>
-  </div>
+  <form>
+      <label for="name">Имя: </label>
+      <input type="text" id="name" v-model="name">
+      <br>
+      <label for="age">Возраст: </label>
+      <input type="number" id="age" v-model="age">
+    </form>
 
-  <div class="product-card">
-    <img class="product-img" :src="product2.src" :alt="product2.alt" />
-    <h2>{{ product2.title }}</h2>
-    <p>{{ product2.description }}</p>
-    <p>Цена: {{ product2.price }}</p>
-  </div>
+    <div v-if="age < 0">
+      <p>Это как</p>
+    </div>
+    <div v-else-if="age < 18">
+      <p>Привет, {{ name }} пока туда сюда двигаемся молодые</p>
+    </div>
+    <div v-else-if="age <= 100">
+      <p>Привет, {{ name }} вы взрослый</p>
+    </div>
+    <div v-else>
+      <p>Агай вам хорошо??</p>
+    </div>
 </template>
-
+  
 <style scoped>
-.product-img {
-  width: 300px;
-  height: 300px;
-}
 </style>
