d6c6fa7f1234d8d7771f30dcf35edb3370590bea
hindian <133589737+hindian-dev@users.noreply.github.com>
2023-10-18 00:37:56 +0600

    zad 4

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,26 @@
 export default {
   data: () => {
     return {
-      colors: [
-        { name: "Красный", code: "#FF0000" },
-        { name: "Зеленый", code: "#00FF00" },
-        { name: "Синий", code: "#0000FF" },
-        { name: "Желтый", code: "#FFFF00" },
-        { name: "Фиолетовый", code: "#800080" },
-        { name: "Оранжевый", code: "#FFA500" },
-        { name: "Розовый", code: "#FFC0CB" },
-      ],
+      inputText: "",
+      formattedText: "",
     };
+  },
+  methods: {
+    formatText() {
+      this.formattedText = this.inputText;
+    },
   },
 };
 </script>
 
 <template>
-  <div class="color-card" v-for="(color, index) in colors" :key="index">
-    <div class="color-box" :style="{backgroundColor: color.code }"></div>
-    <p>{{ color.name }}</p>
-    <p>{{ color.code }}</p>
+  <div>
+    <p>
+      <b><i>{{ formattedText }}</i></b>
+    </p>
   </div>
+  <textarea v-model="inputText" @input="formatText"></textarea>
 </template>
   
 <style scoped>
-.color-card {
-  display: inline-block;
-  width: 150px;
-  margin: 10px;
-  padding: 10px;
-  text-align: center;
-  border: 1px solid #ccc;
-}
 </style>
